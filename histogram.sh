499ed7503c4b27207cb3224213d3dfbbf008730c
denrou <deroussel@gmail.com>
2013-12-30 19:43:29 -0800

    Get file from pipe

--- histogram.sh
+++ histogram.sh
@@ -21,10 +21,14 @@ set -o nounset								# Treat unset variables as an error
 NUMBER_CLASS=10
 # ----------------------
 
-LIST_VALUES=$1
-TMP_STATS=$(mktemp)
+TMP_LIST_VALUES=$(mktemp)
+$(while read line; do echo "$line" >> $TMP_LIST_VALUES; done < "${1:-/proc/${$}/fd/0}")
+# The substitution ${1:-...) takes $1 if defined otherwise the file name of the
+# standard input of the own process is used
 
-# Find bonding limits and store it to a temporary file (min_value, max_value and number of entries)
+# Find bonding limits and store it to a temporary file (min_value, max_value and
+# number of entries)
+TMP_STATS=$(mktemp)
 awk '
 	NR==1{
 		min = $1
@@ -36,7 +40,7 @@ awk '
 	}
 	END{
 		printf( "%f\t%f\t%d\n", min, max, NR )
-	}' $LIST_VALUES > $TMP_STATS
+	}' $TMP_LIST_VALUES > $TMP_STATS
 	
 VALUE_MIN=$(cut -f 1 $TMP_STATS)
 VALUE_MAX=$(cut -f 2 $TMP_STATS)
@@ -58,4 +62,6 @@ awk -v nclass=$NUMBER_CLASS -v valmin=$VALUE_MIN -v step=$STEP '
 		for( i = 0; i < nclass; i++ )
 			if( n[i] > 0 )
 				printf( "%f\t%d\n", class[i], n[i] )
-	}' $LIST_VALUES
+	}' $TMP_LIST_VALUES
+	
+rm $TMP_LIST_VALUES
